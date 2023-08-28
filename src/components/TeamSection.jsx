8626933c9c55dbe7140d6601ab7044731a79bec6
Qoptars-Dev <138315382+Qoptars-Dev@users.noreply.github.com>
2023-08-28 19:40:54 +0530

    removed contact number (#5)
    * removed contact number
    
    * edited social media icons

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -38,7 +38,7 @@ const TeamSection = () => {
             <div key={index} className="rounded-lg p-6 justify-center item-center">
               <img
                 src={member.image}
-                alt={`${member.name} Photo`}
+                alt={`${member.name} Qoptars`}
                 className="h-32 w-32 object-cover rounded-full mb-4 mx-auto"
               />
               <h3 className="text-xl text-center font-Mont font-light text-white mb-2">{member.name}</h3>
@@ -49,9 +49,6 @@ const TeamSection = () => {
                 </a>
                 <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="mr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="white" viewBox="0 0 24 24"><path d="M0 0v24h24v-24h-24zm8 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.397-2.586 7-2.777 7 2.476v6.759z"/></svg>
-                </a>
-                <a href={`tel:${member.phone}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="white" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.5 17.311l-1.76-3.397-1.032.505c-1.12.543-3.4-3.91-2.305-4.497l1.042-.513-1.747-3.409-1.053.52c-3.601 1.877 2.117 12.991 5.8 11.308l1.055-.517z"/></svg>
                 </a>
               </div>
             </div>
